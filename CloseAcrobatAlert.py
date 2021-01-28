f581e6be5d0792a2c893bd6f1e764b6a79a1ba43
xupeng <xupeng@tca.iscas.ac.cn>
2021-01-28 21:52:38 +0800

    ...

--- CloseAcrobatAlert.py
+++ CloseAcrobatAlert.py
@@ -12,7 +12,7 @@ def get_child_windows(parent):
     win32gui.EnumChildWindows(parent, lambda hwnd, param: param.append(hwnd),  hwndChildList)
     return hwndChildList
 
-js_window_exist=False
+
 def close(hwnd, mouse):
     global js_window_exist
     if IsWindow(hwnd) and IsWindowVisible(hwnd):
@@ -23,11 +23,11 @@ def close(hwnd, mouse):
     if IsWindow(hwnd) and IsWindowEnabled(hwnd) and IsWindowVisible(hwnd):
         # classname =GetClassName(hwnd)
         windowtext =GetWindowText(hwnd).decode('gbk')
-        if windowtext.find('Acrobat')>=0 or windowtext.find(u'福昕阅读器')>=0:
+        if windowtext.find('Acrobat')>=0 or windowtext.find(u'福昕阅读器')>=0 or windowtext.startswith('Foxit Reader'):
             cl = get_child_windows(hwnd)
             for c in cl:
                 t = GetWindowText(c).decode('gbk')
-                if t == u'确定' or t.startswith(u'确定'):
+                if t == u'确定' or t.startswith(u'确定') or t.startswith('&OK'):
                     win32gui.SendMessage(c, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, 0)
                     win32gui.SendMessage(c, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, 0)
 
